f89cf396068dfad911a01a4cd5cf83ef38334e1d
belo <69517797+beloele@users.noreply.github.com>
2024-07-25 14:02:35 +0300

    dfgdf
    gdfgfd

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jojobet: Canlı Bahis, Spor Bahisleri ve Casino - Jojobet Giriş",
+  description: " Jojobet, yüksek oranlar ve cazip bonuslarla bahis tutkunlarının gözdesi olmuştur. Futbol, basketbol, tenis gibi popüler spor dallarında geniş bahis seçenekleri sunan site, aynı zamanda canlı bahis imkanı da sunar. Jojobet, spor bahisleri ve casino oyunlarında eşsiz bir deneyim sunar.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
